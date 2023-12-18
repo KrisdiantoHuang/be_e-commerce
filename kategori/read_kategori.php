47ff79a5f97fb4d00612081af41cd7783a4003ce
Ulfasartika <ulfasartika005@gmail.com>
2023-12-18 12:17:34 +0700

    fix update kategori

--- kategori/read_kategori.php
+++ kategori/read_kategori.php
@@ -2,35 +2,53 @@
 include "../env.php";
 
 $res = [
-  "status" => 200,
-  "msg" => "",
-  "body" => [
-    "data" => [
-      "nama_kategori" => "",
-    ]
-  ]
+    "status" => 200,
+    "msg" => "",
+    "body" => [
+        "data" => [],
+    ],
 ];
 
-$q = mysqli_query($conn, "SELECT * FROM kategori_tb");
+// Check if an ID is provided
+if (isset($_GET['id'])) {
+    $id_kategori = $_GET['id'];
 
-// Inisialisasi array untuk menyimpan data
-$dataArray = array();
+    $stmt = $conn->prepare("SELECT * FROM kategori_tb WHERE id_kategori = ?");
+    $stmt->bind_param("i", $id_kategori);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
 
-while ($row = mysqli_fetch_array($q)) {
-    $data = array(
-        'id_kategori' => $row['id_kategori'],
-        'nama_kategori' => $row['nama_kategori']
-    );
-    $dataArray[] = $data;
-}
+    if ($row) {
+        $res['body']['data'] = $row;
+    } else {
+        $res['status'] = 401;
+        $res['msg'] = "Data tidak ditemukan";
+    }
 
-if (!empty($dataArray)) {
-    $res['status'] = 200;
-    $res['msg'] = "Data berhasil diambil";
-    $res['body']['data'] = $dataArray;
+    $stmt->close();
 } else {
-    $res['status'] = 401;
-    $res['msg'] = "Data tidak ditemukan";
+    // Fetch all categories if no ID is provided
+    $q = mysqli_query($conn, "SELECT * FROM kategori_tb");
+
+    // Inisialisasi array untuk menyimpan data
+    $dataArray = array();
+
+    while ($row = mysqli_fetch_array($q)) {
+        $data = array(
+            'id_kategori' => $row['id_kategori'],
+            'nama_kategori' => $row['nama_kategori'],
+        );
+        $dataArray[] = $data;
+    }
+
+    if (!empty($dataArray)) {
+        $res['body']['data'] = $dataArray;
+    } else {
+        $res['status'] = 401;
+        $res['msg'] = "Data tidak ditemukan";
+    }
 }
 
 echo json_encode($res);
+?>
