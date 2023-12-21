0359c11ce38f2b9be2ec84037f4c93b62c8786e0
ameliasyyafitri29 <ameliasyafittrilx8@gmail.com>
2023-12-21 13:22:12 +0700

    060.update_produk

--- produk/update_produk.php
+++ produk/update_produk.php
@@ -2,78 +2,69 @@
 include "../env.php";
 
 $res = [
-  "status" => 200,
-  "msg" => "",
-  "body" => [
-      "data" => [
-          "gambar_brg" => "",
-          "kode_brg" => "",
-          "nama_brg" => "",
-          "harga_brg" => "",
-          "id_kategori" => "",
-          "stok_brg" => "",
-          "deskripsi_brg" => "",
-      ]
-  ]
-      ];
+    "status" => 200,
+    "msg" => "",
+    "body" => [
+        "data" => [
+            "kode_brg" => "",
+            "nama_brg" => "",
+            "harga_brg" => "",
+            "nama_kategori" => "",
+            "stok_brg" => "",
+            "deskripsi_brg" => "",
+            "gambar_brg" => ""
+        ]
+    ]
+];
 
-      // Mengambil informasi file gambar
-$gambar_brg = $_FILES["gambar_brg"]["name"];
-$gambar_tmp = $_FILES["gambar_brg"]["tmp_name"];
-$gambar_path = "gambar/" . $gambar_brg;  // Sesuaikan dengan direktori upload Anda
-// Pindahkan file gambar ke direktori upload
-move_uploaded_file($gambar_tmp, $gambar_path);
-
-
-// $gambar_brg = basename($_FILES["gambar_brg"]["name"]);
-// $target_file = "/gambar". basename($_FILES["gambar_brg"]["name"]);
-// $upload = move_uploaded_file($_FILES["gambar_brg"]["tmp_name"], $target_file);
-
-// $gambar_brg=$_POST['gambar_brg'];
-$kode_brg = $_GET['kode_brg'];
+$kode_brg = $_POST['kode_brg'];
 $nama_brg = $_POST['nama_brg'];
 $harga_brg = $_POST['harga_brg'];
-$id_kategori = $_POST['id_kategori'];
+$nama_kategori = $_POST['nama_kategori'];
 $stok_brg = $_POST['stok_brg'];
 $deskripsi_brg = $_POST['deskripsi_brg'];
 
-$i=mysqli_query($conn, "SELECT * FROM kategori_tb");
+// Check if the file is uploaded
+if (isset($_FILES['gambar_brg']['name']) && $_FILES['gambar_brg']['name'] != "") {
+    $q = mysqli_query($conn, "SELECT gambar_brg FROM barang_tb WHERE kode_brg='$kode_brg'");
+    $ary = mysqli_fetch_array($q);
+    $gambar = $ary['gambar_brg'];
 
-while ($row = mysqli_fetch_array($i)) {
+    // Unlink only if the file exists
+    if (file_exists("gambar/" . $gambar)) {
+        unlink("gambar/" . $gambar);
+    }
 
-   $row['nama_kategori'];
+    $nama_gbr_baru = basename($_FILES["gambar_brg"]["name"]);
+    $target_file = "gambar/" . basename($_FILES["gambar_brg"]["name"]);
+    $upload = move_uploaded_file($_FILES["gambar_brg"]["tmp_name"], $target_file);
 
+    // Update the database with the new image name
+    mysqli_query($conn, "UPDATE barang_tb SET gambar_brg='$nama_gbr_baru' WHERE kode_brg='$kode_brg'");
 }
 
+// Update other fields
+$query = "UPDATE barang_tb SET nama_brg='$nama_brg', harga_brg='$harga_brg', nama_kategori='$nama_kategori',
+stok_brg='$stok_brg', deskripsi_brg='$deskripsi_brg' WHERE kode_brg='$kode_brg'";
 
-$q= "UPDATE barang_tb SET gambar_brg='$gambar_brg', nama_brg='$nama_brg', id_kategori='$id_kategori',
-harga_brg='$harga_brg',stok_brg='$stok_brg',deskripsi_brg ='$deskripsi_brg'  WHERE kode_brg='$kode_brg'";
-// $q="INSERT INTO barang_tb (gambar_brg,kode_brg,nama_brg,harga_brg,id_kategori,stok_brg,deskripsi_brg) 
-// VALUES ('$gambar_brg','$kode_brg','$nama_brg','$harga_brg','$id_kategori','$stok_brg','$deskripsi_brg')";
+$result = mysqli_query($conn, $query);
 
-
-$result=mysqli_query($conn,$q);
-                                                                      
 if ($result) {
-
-  $res['status'] = 200;
-  $res['msg'] = "Data berhasil di update";
-  $res['body']['data'] =[
-  'gambar_brg' => $gambar_brg,
-  'kode_brg' => $kode_brg,
-  'nama_brg' => $nama_brg,
-  'harga_brg' => $harga_brg,
-  'id_kategori' => $id_kategori,
-  'stok_brg' => $stok_brg,
-  'deskripsi_brg' => $deskripsi_brg,
-  
-  ];
- 
+    $res['status'] = 200;
+    $res['msg'] = "Data berhasil diupdate";
+    $res['body']['data'] = [
+        'gambar_brg' => $nama_gbr_baru,
+        'kode_brg' => $kode_brg,
+        'nama_brg' => $nama_brg,
+        'harga_brg' => $harga_brg,
+        'nama_kategori' => $nama_kategori,
+        'stok_brg' => $stok_brg,
+        'deskripsi_brg' => $deskripsi_brg,
+    ];
 } else {
-  $res['status'] = 401;
-  $res['msg'] = "Gagal update barang";
-  $res['body']['error'] = "Kesalahan validasi input";
+    $res['status'] = 401;
+    $res['msg'] = "Gagal mengupdate data";
+    $res['body']['error'] = "Kesalahan validasi input";
 }
 
 echo json_encode($res);
-?>
