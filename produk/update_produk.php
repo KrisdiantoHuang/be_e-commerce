4eb3eeca790e3fcc5c384b445427384b71e1adc7
muliarizkyadinda <mulia.adnda@gmail.com>
2024-01-16 00:50:31 +0700

    Fix BE

--- produk/update_produk.php
+++ produk/update_produk.php
@@ -17,22 +17,24 @@ $res = [
     ]
 ];
 
-$kode_brg = $_POST['kode_brg'];
-$nama_brg = $_POST['nama_brg'];
-$harga_brg = $_POST['harga_brg'];
-$nama_kategori = $_POST['nama_kategori'];
-$stok_brg = $_POST['stok_brg'];
-$deskripsi_brg = $_POST['deskripsi_brg'];
+$kode_brg = isset($_POST['kode_brg']) ? $_POST['kode_brg'] : '';
+$nama_brg = isset($_POST['nama_brg']) ? $_POST['nama_brg'] : '';
+$harga_brg = isset($_POST['harga_brg']) ? $_POST['harga_brg'] : '';
+$nama_kategori = isset($_POST['nama_kategori']) ? $_POST['nama_kategori'] : '';
+$stok_brg = isset($_POST['stok_brg']) ? $_POST['stok_brg'] : '';
+$deskripsi_brg = isset($_POST['deskripsi_brg']) ? $_POST['deskripsi_brg'] : '';
+$nama_gbr_baru = ''; // Initialize this variable
 
 // Check if the file is uploaded
 if (isset($_FILES['gambar_brg']['name']) && $_FILES['gambar_brg']['name'] != "") {
     $q = mysqli_query($conn, "SELECT gambar_brg FROM barang_tb WHERE kode_brg='$kode_brg'");
     $ary = mysqli_fetch_array($q);
-    $gambar = $ary['gambar_brg'];
+    $gambar = isset($ary['gambar_brg']) ? $ary['gambar_brg'] : '';
 
-    // Unlink only if the file exists
-    if (file_exists("gambar/" . $gambar)) {
-        unlink("gambar/" . $gambar);
+    // Unlink only if the file exists and it is a file, not a directory
+    $file_path = "gambar/" . $gambar;
+    if ($gambar && file_exists($file_path) && is_file($file_path)) {
+        unlink($file_path);
     }
 
     $nama_gbr_baru = basename($_FILES["gambar_brg"]["name"]);
@@ -50,7 +52,6 @@ stok_brg='$stok_brg', deskripsi_brg='$deskripsi_brg' WHERE kode_brg='$kode_brg'"
 $result = mysqli_query($conn, $query);
 
 if ($result) {
-    $res['status'] = 200;
     $res['msg'] = "Data berhasil diupdate";
     $res['body']['data'] = [
         'gambar_brg' => $nama_gbr_baru,
